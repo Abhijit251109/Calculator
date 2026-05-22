9ebb2949aaf6e2ed19c6795c64bcf78dbbfd11e7
Abhijit Singh <abhijitschool036@gmail.com>
2026-05-22 18:11:11 +0530

    second commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,92 +4,65 @@ public class Calculator {
 
     public static void main(String[] args) {
 
+            
+            int[] input = {1, 2, 3, 4};
+            for (var i : input) {
+                    System.out.println(i + ":");
+                }
+
+
         String choose_num = "For addition choose 1, " +
-                "For substraction choose 2, " +
-                "For multiplication choose 3, " +
-                "For division choose 4: ";
-        System.out.println(choose_num);
+                        "For substraction choose 2, " +
+                        "For multiplication choose 3, " +
+                        "For division choose 4, " +
+                        "0 for Termination: ";
+        // System.out.println(choose_num);
 
-        int[] input = {1, 2, 3, 4};
-//        for (var i : input) {
-//            System.out.println(i + ":");
-//        }
-
-        int user_input;
         Scanner scan = new Scanner(System.in);
-        user_input = scan.nextInt();
-//        System.out.println("You choose " + user_input + ": ");
-
+        int user_input;
         int num1, num2;
-        Scanner scan1 = new Scanner(System.in);
 
-        System.out.print("Enter first number: ");
-        num1 = scan1.nextInt();
+        while (true) {
 
-        System.out.print("Enter second number: ");
-        num2 = scan1.nextInt();
+                try{
+            System.out.println(choose_num);
+            System.out.print("Choose (0-4): ");
+            user_input = scan.nextInt();
 
-//        System.out.print("Enter first number: ");
-//        System.out.println(num1);
-//
-//        System.out.print("Enter second number: ");
-//        System.out.println(num2);
-
-
-        switch (user_input) {
-
-            case 1:
-//                int input1, input2;
-//                input1 = scan.nextInt();
-//                input2 = scan.nextInt();
-//
-//                System.out.print("Enter first number: ");
-//                System.out.println(input1);
-//                System.out.print("Enter second number: ");
-//                System.out.println(input2);
-                System.out.println("You chose addition");
-                System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+            if (user_input == 0) {
+                System.out.println("Terminating the program...");
                 break;
+            }
 
+            if (user_input >= 1 && user_input <= 4) {
+                System.out.print("Enter first number: ");
+                num1 = scan.nextInt();
+                System.out.print("Enter second number: ");
+                num2 = scan.nextInt();
 
-            case 2:
-//                int input3, input4;
-//                input3 = scan.nextInt();
-//                input4 = scan.nextInt();
-//
-//                System.out.print("Enter first number: ");
-//                System.out.println(input3);
-//                System.out.print("Enter second number: ");
-//                System.out.println(input4);
-                System.out.println("You chose substraction");
-                System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
-                break;
+                if (user_input == 1) {
+                    System.out.println("You chose Addition.");
+                    System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+                } else if (user_input == 2) {
+                    System.out.println("You chose Substraction.");
+                    System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+                } else if (user_input == 3) {
+                    System.out.println("You chose Multiplication.");
+                    System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+                } else {
+                    System.out.println("You chose Division.");
+                    System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+                }
+            } else {
+                System.out.println("Invalid input, please choose a number between 0 and 4.");
+            }
 
-            case 3:
-//                int input5, input6;
-//                input5 = scan.nextInt();
-//                input6 = scan.nextInt();
-//
-//                System.out.print("Enter first number: ");
-//                System.out.println(input5);
-//                System.out.print("Enter second number: ");
-//                System.out.println(input6);
-                System.out.println("You chose multiplication");
-                System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
-                break;
-
-            case 4:
-//                int input7, input8;
-//                input7 = scan.nextInt();
-//                input8 = scan.nextInt();
-//
-//                System.out.print("Enter first number: ");
-//                System.out.println(input7);
-//                System.out.print("Enter second number: ");
-//                System.out.println(input8);
-                System.out.println("You chose division");
-                System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
-                break;
+                } catch (Exception e) {
+                    System.out.println("Invalid input, please enter a valid number." + "\n");
+                    scan.next();
+                }
         }
+
+        scan.close();
     }
 }
